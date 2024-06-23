16623b5e3f02e97de2e1c9b6a54d1e7301bb9946
Deep <sekharsmemories@gmail.com>
2024-06-23 08:41:07 +0530

    update

---
+++ cf.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double lld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef vector<int> vii;
+typedef vector<ll> vll;
+typedef vector<vector<int>> vvii;
+typedef vector<vector<ll>> vvll;
+typedef vector<vector<pii>> vvpii;
+typedef vector<vector<pll>> vvpll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef unordered_map<ll, ll> uomap;
+#define fio                           \
+	ios_base::sync_with_stdio(false); \
+	cin.tie(NULL);                    \
+	cout.tie(NULL);
+#define yes cout << "yes\n";
+#define Yes cout << "Yes\n";
+#define YES cout << "YES\n";
+#define no cout << "no\n";
+#define No cout << "No\n";
+#define NO cout << "NO\n";
+#define NL cout << "\n";
+#define ckk cout << " = ";
+#define MN cout << "-1\n";
+#define all(k) k.begin(), k.end()
+#define c(i, v) for (auto &i : v)
+#define rep(x, a, b) for (int x = a; x < b; x++)
+#define repl(x, a, b) for (long long int x = a; x < b; x++)
+#define F first
+#define S second
+#define pb push_back
+#define RT return;
+const ll mod = 1e9 + 7;
+// helper functions
+ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+
+void BINDAS()
+{
+
+
+
+}
+
+int main()
+{
+	fio int tc;
+	// tc = 1;
+	cin >> tc;
+	while (tc--)
+	{
+		BINDAS();
+	}
+	return 0;
+}
