7bf63282b0556de8c15c00735ef2d9ada86b0ad0
Deep Sekhar <sekharsmemories@gmail.com>
2025-07-29 15:17:21 +0530

    automated commit

--- tips/primes.cpp
+++ tips/primes.cpp
@@ -1,12 +1,11 @@
-        vector<int> spf(mx+1);
-        iota(begin(spf), end(spf), 0);
-        for(long long i = 2; i * i <= mx; ++i) {
-            if (spf[i] == i) {
-                for (long long j = i * i; j <= mx; j += i) {
-                    if(spf[j] == j)
-                    spf[j] = i;
-                }
-            }
+vector<int> spf(mx+1);
+iota(begin(spf), end(spf), 0);
+for(long long i = 2; i * i <= mx; ++i) {
+    if (spf[i] == i) {
+        for (long long j = i * i; j <= mx; j += i) {
+            if(spf[j] == j)
+            spf[j] = i;
         }
+    }
 
-        auto isPrime = [&](int num){return num>=2 and spf[num]==num;};
+auto isPrime = [&](int num){return num>=2 and spf[num]==num;};
